6cfa1b1ab58defde57d7bfabaa8432d4f56bf9e6
nnakki <yvely225@gmail.com>
2022-04-21 03:25:34 +0900

    [feat] userInfo  html 페이지 추가 / API 추가
    - userInfo htmll 페이지 및 css 스타일 시트 추가
    - app.py에 userInfo API 추가 (수정예정)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
-
 from flask import Flask, render_template, jsonify, request
+
+import riotapi
 
 app = Flask(__name__)
 
@@ -12,6 +13,29 @@ db = client.dbchallenger
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/api/member', methods=['GET'])
+def userInfo(userName):
+
+    userName = request.form['userName_give']    ## 누를 때 userName 받아오기..?
+
+    riotData = riotapi.RiotApi()
+
+    userData = riotData.getUserRankInfo(userName)      #티어
+    tier = userData['tier']
+    rank = userData['rank']
+
+    lastGames = riotData.getUserLastGames(userName)    #최근전적
+
+    return jsonify({'tier':tier, 'rank':rank, 'lastGames':lastGames})
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
