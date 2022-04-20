bd5b4a12203d489f6378409623b3c910ac8afcc7
유시준 <simba0502@naver.com>
2022-04-20 16:09:55 +0900

    [feat] 유저 티어 조회/유저 최근 게임 기록 조회 메서드 구현

---
+++ riotapi.py
@@ -0,0 +1,42 @@
+import requests
+from urllib import parse
+
+class RiotApi:
+    API_KEY = "RGAPI-d4d1580e-fae9-4653-b5bd-2de5e488dce4"
+    REQUEST_HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+        "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "https://developer.riotgames.com",
+        "X-Riot-Token": API_KEY
+    }
+
+    def getUserInfo(self, userNickname):
+        encodedName = parse.quote(userNickname)
+        player = requests.get("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + encodedName,headers=self.REQUEST_HEADERS).json();
+        return player;
+
+    def getUserRankInfo(self, userNickname):
+        lolUserInfoConsistOfJson = self.getUserInfo(userNickname);
+        id = lolUserInfoConsistOfJson["id"];
+        playerInfo = requests.get("https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/"+id, headers = self.REQUEST_HEADERS).json();
+        return playerInfo
+
+    def getUserLastGameId(self,puuid):
+        gameIdList = requests.get("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?type=ranked&start=0&count=5", headers = self.REQUEST_HEADERS).json();
+        return gameIdList;
+
+    def getUserLastGames(self,userNickname):
+        lolUserInfoConsistOfJson = self.getUserInfo(userNickname);
+        puuid = lolUserInfoConsistOfJson["puuid"];
+        userLastGameIdList = self.getUserLastGameId(puuid);
+        gameList =[];
+        for gameId in userLastGameIdList:
+            gameInfo = requests.get("https://asia.api.riotgames.com/lol/match/v5/matches/"+gameId, headers = self.REQUEST_HEADERS).json();
+            participantsList =gameInfo["info"]["participants"]
+            for participant in participantsList:
+                name = participant["summonerName"]
+                if name==userNickname:
+                    gameList.append(participant);
+                    break;
+        return gameList;
